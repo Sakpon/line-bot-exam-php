1ba5e07158202bb45541db2c784e80a69ebef43b
Sakpon <42953385+Sakpon@users.noreply.github.com>
2018-09-16 22:34:34 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '4zvHG1lNOmD6SNhlNuT7HMg6bLmPMfdYcSCzuzPdCkVogXDpM8IOsgJYKotgMDhIcQA26zVG2r5Oxyg2EUFC5y1exm7PViDP8tOoYF17qzY4RmcKm3t+GZyhqNsPd51rlHzwadn5AQ6s54ozBpGm8ZpJanU2lNBfYDNdOKTsp30=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
