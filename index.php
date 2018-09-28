8b7cc4d8573f0781b5f69a758f0020db3312363d
Sakpon <42953385+Sakpon@users.noreply.github.com>
2018-09-28 14:11:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
-
+<!-- LINE Tag Base Code -->
+<!-- Do Not Modify -->
+<script>
+(function(g,d,o){
+  g._ltq=g._ltq||[];g._lt=g._lt||function(){g._ltq.push(arguments)};
+  var h=location.protocol==='https:'?'https://d.line-scdn.net':'http://d.line-cdn.net';
+  var s=d.createElement('script');s.async=1;
+  s.src=o||h+'/n/line_tag/public/release/v1/lt.js';
+  var t=d.getElementsByTagName('script')[0];t.parentNode.insertBefore(s,t);
+    })(window, document);
+_lt('init', {
+  customerType: 'lap',
+  tagId: 'b4a7d799-cdb6-4551-8bae-78b2b9f44fda'
+});
+_lt('send', 'pv', ['b4a7d799-cdb6-4551-8bae-78b2b9f44fda']);
+</script>
+<noscript>
+  <img height="1" width="1" style="display:none"
+       src="https://tr.line.me/tag.gif?c_t=lap&t_id=b4a7d799-cdb6-4551-8bae-78b2b9f44fda&e=pv&noscript=1" />
+</noscript>
+<!-- End LINE Tag Base Code -->
 
 echo "Hello world of LINE BOT";
+
 
 
 
